05b68841b1def0ef8d50e6264c86759d60e3731e
pakkk <paco.benitez.chico@gmail.com>
2024-04-18 17:11:21 +0200

    Swagger UI + ejecucion como servicio

--- src/main/resources/scripts/reaktor_timetableserver.sh
+++ src/main/resources/scripts/reaktor_timetableserver.sh
@@ -9,12 +9,6 @@ JAR_NAME="TimetableServer-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 # Archivo de marca para almacenar la última fecha de modificación del JAR
 LAST_MODIFIED_FILE="$JAR_PATH/last_modified.txt"
 
-# Nombre del servicio
-SERVICE_NAME="timetableserver.service"
-
-# Archivo de bloqueo
-LOCK_FILE="/var/lock/$SERVICE_NAME.lock"
-
 # Función para obtener la fecha de modificación actual del JAR
 get_jar_last_modified() {
     stat -c %Y "$JAR_PATH/$JAR_NAME"
@@ -41,48 +35,32 @@ check_jar_changes() {
     if [ "$current_modified" -gt "$last_modified" ]; then
         
         echo "El JAR ha cambiado. Reiniciando el servicio..."
-
-		# Bloquear el archivo para evitar que se ejecute el reinicio más de una vez simultáneamente
-        if flock -n 9; then
-
-            systemctl restart "$SERVICE_NAME"
-
-        else
-
-            echo "No se pudo bloquear el reinicio del servicio. Otro reinicio ya está en curso."
-
-        fi
         
-    fi
-
-    # Actualizar la marca con la nueva fecha de modificación
-    echo "$current_modified" > "$LAST_MODIFIED_FILE"
-
+        # Detener el proceso si está en ejecución
+        pkill -f "$JAR_NAME"
+        
+        # Iniciar el proceso nuevamente
+        java -jar "$JAR_PATH/$JAR_NAME" --spring.profiles.active=VPS &
+        
+        # Actualizar la marca con la nueva fecha de modificación
+        echo "$current_modified" > "$LAST_MODIFIED_FILE"
+        
     fi
 }
 
 # Función para verificar si el proceso está en ejecución
 check_process() {
 
-    if ! pgrep -f "$JAR_NAME" > /dev/null; then
+	if ! pgrep -f "$JAR_NAME" > /dev/null; then
 
-        echo "El proceso no está en ejecución. Reiniciando el servicio..."
+        echo "El proceso no está en ejecución. Iniciando el servicio..."
 
-        # Bloquear el archivo para evitar que se ejecute el reinicio más de una vez simultáneamente
-        if flock -n 9; then
+        # Iniciar el proceso si no está en ejecución
+        java -jar "$JAR_PATH/$JAR_NAME" --spring.profiles.active=VPS &
 
-            systemctl restart "$SERVICE_NAME"
-
-        else
-
-            echo "No se pudo bloquear el reinicio del servicio. Otro reinicio ya está en curso."
-
-        fi
     fi
 }
 
 # Verificar cambios en el JAR y estado del proceso
 check_jar_changes
-
-# Redirigir el descriptor de archivo para el bloqueo, evitando que se produzcan conflictos entre las ejecuciones de check_jar_changes y check_process
-check_process 9>"$LOCK_FILE"
+check_process
