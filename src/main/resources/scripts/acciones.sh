326813456c26309d1734764b4e97ffcad995b242
pakkk <paco.benitez.chico@gmail.com>
2024-04-18 18:12:44 +0200

    Swagger UI + ejecucion como servicio

--- src/main/resources/scripts/acciones.sh
+++ src/main/resources/scripts/acciones.sh
@@ -1,7 +1,9 @@
 cp timetableserver.service /etc/systemd/system
+cp timetableserver.timer /etc/systemd/system
 
 chmod 777 /etc/systemd/system/timetableserver.service
 chmod 777 reaktor_timetableserver.sh
+chmod 777 /etc/systemd/system/timetableserver.timer
 
 # Añadir al arranque del servidor
 sudo systemctl start timetableserver.service
@@ -11,3 +13,9 @@ sudo systemctl enable timetableserver.service
 
 # Ver el estado del servicio
 sudo systemctl status timetableserver.service
+
+# Recargar systemd para que reconozca los nuevos archivos de configuración
+sudo systemctl daemon-reload
+
+# Habilitar y comenzar el temporizador
+sudo systemctl enable --now timetableserver.timer
