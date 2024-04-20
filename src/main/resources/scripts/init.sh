6c0118ac87f5dce3a29ad4e80bc09fbb54f8b6c5
pakkk <paco.benitez.chico@gmail.com>
2024-04-20 12:38:12 +0200

    Actualizado el script de arranque

--- src/main/resources/scripts/init.sh
+++ src/main/resources/scripts/init.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
+
+if [ ! -d "$DIRECTORIO" ]; then
+    mkdir /root/servicios
+fi
+
+cp reaktor_timetableserver.sh /root/servicios
+chmod 777 /root/servicios/reaktor_timetableserver.sh
+
 cp timetableserver.service /etc/systemd/system
 cp timetableserver.timer /etc/systemd/system
 
-chmod 777 /etc/systemd/system/timetableserver.service
-chmod 777 reaktor_timetableserver.sh
-chmod 777 /etc/systemd/system/timetableserver.timer
+# Deshabilitar si existe el temporizador
+sudo systemctl disable timetableserver.timer
+
+# Parar el timer
+sudo systemctl stop timetableserver.timer
 
 # Desabilitar si existe el servicio
-sudo systemctl disabled timetableserver.service
+sudo systemctl disable timetableserver.service
 
 # Parar el servicio
 sudo systemctl stop timetableserver.service
@@ -16,9 +27,6 @@ sudo systemctl start timetableserver.service
 
 # Arrancar el servicio
 sudo systemctl enable timetableserver.service
-
-# Ver el estado del servicio
-sudo systemctl status timetableserver.service
 
 # Recargar systemd para que reconozca los nuevos archivos de configuración
 sudo systemctl daemon-reload
