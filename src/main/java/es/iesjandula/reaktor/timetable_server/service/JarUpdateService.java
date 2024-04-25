95f4792cbc789fcf8ae155f7fcdd1fe3b13d3d92
pakkk <paco.benitez.chico@gmail.com>
2024-04-25 11:32:12 +0200

    Añadiendo controlador de cambios de JAR

---
+++ src/main/java/es/iesjandula/reaktor/timetable_server/service/JarUpdateService.java
@@ -0,0 +1,44 @@
+package es.iesjandula.reaktor.timetable_server.service;
+
+import java.io.File;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class JarUpdateService
+{
+    private long lastModified;
+
+    @PostConstruct
+    public void init()
+    {
+        // Obtener la ruta del archivo JAR en ejecución
+        String jarFilePath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        File jarFile = new File(jarFilePath);
+
+        // Obtener la fecha de modificación del archivo JAR
+        this.lastModified = jarFile.lastModified();
+    }
+
+    @Scheduled(fixedRate = 5000) // Ejecutar cada 5 segundos
+    public void checkJarUpdate()
+    {
+        // Obtener la ruta del archivo JAR en ejecución
+        String jarFilePath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        File jarFile = new File(jarFilePath);
+
+        // Verificar si el archivo JAR ha sido actualizado
+        if (jarFile.lastModified() > this.lastModified)
+        {
+            log.info("¡El JAR ha sido actualizado! Finalizando la aplicación...");
+
+            // Salir de la aplicación
+            System.exit(0);
+        }
+    }
+}
