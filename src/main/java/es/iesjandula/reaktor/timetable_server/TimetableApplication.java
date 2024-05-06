bdd4b33d9474a7b0be3ce0c938f7988de2a01737
pakkk <paco.benitez.chico@gmail.com>
2024-05-06 19:03:04 +0200

    cambios en launcher

--- src/main/java/es/iesjandula/reaktor/timetable_server/TimetableApplication.java
+++ src/main/java/es/iesjandula/reaktor/timetable_server/TimetableApplication.java
@@ -3,7 +3,10 @@ package es.iesjandula.reaktor.timetable_server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -13,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
  *
  */
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
+@EntityScan(basePackages = "es.iesjandula.timetable_server")
 @ComponentScan( basePackages = "es.iesjandula.reaktor.timetable_server")
 public class TimetableApplication implements CommandLineRunner
 {
