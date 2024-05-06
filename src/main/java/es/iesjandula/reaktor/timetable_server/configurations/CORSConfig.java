a0e0d9c682200d3f18a3fe1355bff2cb9270e056
pakkk <paco.benitez.chico@gmail.com>
2024-05-06 10:53:31 +0200

    Correcciones del código de Pablo. Mientras no estén las BBDD no se deben introducir dependencias en el POM. Además, no puso una anotación en el CORSConfig

--- src/main/java/es/iesjandula/reaktor/timetable_server/configurations/CORSConfig.java
+++ src/main/java/es/iesjandula/reaktor/timetable_server/configurations/CORSConfig.java
@@ -3,9 +3,11 @@ package es.iesjandula.reaktor.timetable_server.configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CORSConfig implements WebMvcConfigurer
 {
 	@Value("${urlCors}")
